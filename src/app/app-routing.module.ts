27600b7c7567149a2dc42a568538a086218713f2
Denssed <rodriguezp.jeremys@gmail.com>
2023-01-13 09:34:35 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const routes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  // {path:'dashboard',component:DashboardComponent},
+  // {path:'login', component:LoginComponent},
+  // {path:'cart', component:CartComponent},
+  // {path:'test1', component:testComponent},
+  // {path:'test2', component:testComponent},
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
